11f541c69bf34aad3548049101b7e96be592ec53
Alexandre Serre <72320006+Alexandre-Pikoo-Serre@users.noreply.github.com>
2021-04-30 22:58:37 +0200

    Pause/GameOver screen
    - Prefab: Pause/GameOver screen (Continuer, Recommencer, Quitter)
    - Prefab: GameManager

---
+++ Assets/03_Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private PauseManager pauseScreen;
+
+    public static bool isPause;
+    public static bool isGameOver;
+
+    /* Singleton */
+    public static GameManager instance;
+
+    void Awake()
+    {
+        /* Singleton */
+        if (instance != null)
+        {
+            Debug.LogError("Plus d'une instance GameManager existe!");
+            return;
+        }
+
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (isGameOver)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseScreen.ShowPauseScreen(PauseManager.TypeOfPause.Pause, !isPause);
+    }
+
+    public void GameOver()
+    {
+        if (GameManager.isGameOver)
+            return;
+
+        isGameOver = true;
+
+        pauseScreen.ShowPauseScreen(PauseManager.TypeOfPause.GameOver, true);
+    }
+}
---
+++ Assets/03_Scripts/Menu/PauseManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    public enum TypeOfPause
+    {
+        Both,
+        Pause,
+        GameOver
+    }
+
+    [SerializeField] private Image panel;
+    [SerializeField] private Text title;
+    [SerializeField] private GameObject buttonResume;
+
+    private Color gameOverPanelColor = new Color32(255, 0, 18, 100); //new Color(1f, 0f, 7.06f, 35.22f); // RGB(255,0,18,100)
+    private Color pausePanelColor = new Color32(125, 125, 125, 100); //new Color(49f, 49f, 49f, 35.22f); // RGB(125,125,125,100)
+
+    public void ShowPauseScreen(TypeOfPause type, bool isShow)
+    {
+        if (gameObject == null)
+            return;
+
+        switch (type)
+        {
+            case TypeOfPause.GameOver:
+                GameManager.isGameOver = isShow;
+                panel.color = gameOverPanelColor;
+                title.text = "Game Over";
+                buttonResume.SetActive(false);
+                break;
+
+            case TypeOfPause.Pause:
+                GameManager.isPause = isShow;
+                panel.color = pausePanelColor;
+                title.text = "Pause";
+                buttonResume.SetActive(true);
+                break;
+
+            case TypeOfPause.Both:
+                GameManager.isGameOver = isShow;
+                GameManager.isPause = isShow;
+                break;
+            default:
+                break;
+        }
+
+        gameObject.SetActive(isShow);
+
+    }
+
+    public void Resume()
+    {
+        ShowPauseScreen(TypeOfPause.Both, false);
+    }
+
+    public void Retry()
+    {
+        ShowPauseScreen(TypeOfPause.Both, false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
