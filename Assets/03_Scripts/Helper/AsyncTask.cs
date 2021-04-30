65f71d0c641b06adc0e1cc262b4a25427b36b759
Bubulle <46298047+Bubulleux@users.noreply.github.com>
2021-04-30 22:18:14 +0200

    AsyncTask

---
+++ Assets/03_Scripts/Helper/AsyncTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using System;
+
+public class AsyncTask : MonoBehaviour
+{
+    public static DateTime lastRender;
+    private static double maxTimeWait = 0.5d;
+
+
+    private void FixedUpdate()
+    {
+        lastRender = DateTime.Now;
+    }
+    public static async Task DelayIfNeed(int timeToWait)
+    {
+        if ((DateTime.Now - lastRender).TotalSeconds >= maxTimeWait)
+        {
+            await Task.Delay(timeToWait);
+        }
+    }
+
+    public static async Task MonitorTask(Task task)
+    {
+        while(!task.IsCompleted && !task.IsFaulted)
+        {
+            await Task.Delay(1);
+        }
+        if(task.IsFaulted)
+        {
+            Debug.LogException(task.Exception);
+        }
+    }
+
+}
