960800d8d0c7a578049324d452212ae5ed9b4bd3
Bubulle <46298047+Bubulleux@users.noreply.github.com>
2021-05-01 21:59:45 +0200

    fix PlayerControler

--- Assets/03_Scripts/Fight/PlayerControler.cs
+++ Assets/03_Scripts/Fight/PlayerControler.cs
@@ -12,7 +12,7 @@ public class PlayerControler : Controler
 
 	public override void InitTurn()
 	{
-		UIManager.instance.inventory.RefreshDiceBar(this);
+		UIManager.instance.inventory.RefreshDiceBar(motor.dices, this);
 	}
 
 	public void PlayDice(Dice dice)
@@ -20,7 +20,7 @@ public class PlayerControler : Controler
 		if (motor.myTurn)
 		{
 			motor.PlayDice(dice);
-			UIManager.instance.inventory.RefreshDiceBar(this);
+			UIManager.instance.inventory.RefreshDiceBar(motor.dices, this);
 		}
 	}
 }
