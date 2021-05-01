4f676a81c5db682d52b32080098a1ede728c54e3
Bubulle <46298047+Bubulleux@users.noreply.github.com>
2021-05-01 23:37:33 +0200

    Enemy, Chest

--- Assets/03_Scripts/EnnemyScriptOW.cs
+++ Assets/03_Scripts/EnnemyScriptOW.cs
@@ -3,18 +3,25 @@ using UnityEngine.UI;
 
 public class EnnemyScriptOW : MonoBehaviour
 {
-    private Text interactUI;
     private bool isInRange;
+    public EnemyData enemyData;
+
+    public int spritIndex = 0;
+    public float animationCooldown = 0f;
     
-    private void Awake()
-    {
-        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
-    }
 
     private void Update()
     {
+        animationCooldown -= Time.deltaTime;
+        if (animationCooldown <= 0f)
+		{
+            animationCooldown = 1f;
+            spritIndex = (spritIndex + 1) % enemyData.sprites.Length;
+            GetComponent<SpriteRenderer>().sprite = enemyData.sprites[spritIndex];
+		}
+
         if (Input.GetKeyDown(KeyCode.E)  && isInRange)
-        {            
+        {
             GameManager.instance.LoadCombatScene(this);
         }
     }
@@ -23,7 +30,7 @@ public class EnnemyScriptOW : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            interactUI.enabled = true;
+            InteractUI.instance.SetText("Appuie sur 'E' pour te battre avec" + enemyData.name);
             isInRange = true;
         }
     }
@@ -32,7 +39,7 @@ public class EnnemyScriptOW : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            interactUI.enabled = false;
+            InteractUI.instance.SetText("");
             isInRange = false;
         }
     }
---
+++ Assets/03_Scripts/InteractUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractUI : MonoBehaviour
+{
+    public static InteractUI instance;
+	public Text textInfo;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	public void SetText(string text)
+	{
+		textInfo.text = text;
+	}
+}
