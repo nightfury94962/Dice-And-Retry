80ba552153d2421b07c330df412e1e03e041ddd3
misfitonie <misfitonie@gmail.com>
2021-05-01 17:24:56 +0200

    add ennemy

---
+++ Assets/03_Scripts/EnnemyScriptOW.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnnemyScriptOW : MonoBehaviour
+{
+    private Text interactUI;
+    private bool isInRange;
+    
+    private void Awake()
+    {
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)  && isInRange)
+        {
+            SceneManager.LoadScene("Combat");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactUI.enabled = true;
+            isInRange = true;
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactUI.enabled = false;
+            isInRange = false;
+        }
+    }
+}
