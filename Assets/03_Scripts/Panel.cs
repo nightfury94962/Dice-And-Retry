17ad8646de32d35e59650f5686f66db52f715611
Alexandre Serre <72320006+Alexandre-Pikoo-Serre@users.noreply.github.com>
2021-05-01 20:33:14 +0200

    Panneaux UI #34
    Panneaux UI #34

---
+++ Assets/03_Scripts/Panel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour
+{
+    [SerializeField] private string message;
+    private Text interactUI;
+
+    private void Awake()
+    {
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactUI.enabled = true;
+            interactUI.text = message;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactUI.text = string.Empty;
+            interactUI.enabled = false;
+        }
+    }
+}
