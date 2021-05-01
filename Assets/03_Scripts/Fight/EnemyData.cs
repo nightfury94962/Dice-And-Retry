53e5ad5e1aa9082fdaf4a1d4f3944d729d4adfad
Bubulle <46298047+Bubulleux@users.noreply.github.com>
2021-05-01 17:52:17 +0200

    Enemy, Dice Scriptabel Object

---
+++ Assets/03_Scripts/Fight/EnemyData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Dice And Retry/Enemy Data")]
+public class EnemyData : ScriptableObject
+{
+    public string name;
+    public int life;
+    public Sprite[] sprites;
+
+    public DiceData[] customDice;
+
+    public int[] randomDiceCount = new int[3];
+}
